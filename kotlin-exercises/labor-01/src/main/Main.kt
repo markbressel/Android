8f7ea0721dcbf8aaff794438bbfe4a989f008b5d
markbressel <markbressel@gmail.com>
2024-09-26 14:41:33 +0300

    Lab 1 done

--- kotlin-exercises/labor-01/src/main/Main.kt
+++ kotlin-exercises/labor-01/src/main/Main.kt
@@ -1,5 +1,7 @@
 package main
 
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
     println("1. Write a main function that adds two values (immutable variables) and prints the result using a\n" +
             "String template in the following format: 2 + 3 = 5.")
@@ -67,7 +69,71 @@ fun main(args: Array<String>) {
             "Using map, perform the following operations:\n" +
             "● Double the elements of a list of integers and print it.\n")
     val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(doubleElements(list))
+    doubleElements(list)
+    println("\n")
+    println("● Print the days of week capitalized (e.g. MONDAY for Monday)\n")
+    capitalizedLetters(daysOfWeek)
+    println("\n")
+    println("● Print the first character of each day capitalized (e.g. m for Monday)\n")
+    firstLetter(daysOfWeek)
+    println("\n")
+    println("● Print the length of days (number of characters, e.g. Monday → 6)\n")
+    sizeLetters(daysOfWeek)
+    println("\n")
+    println("● Compute the average length of days (in number of characters)\n")
+    println(averageLength(daysOfWeek))
+    println("\n")
+    println("7. Mutable lists.\n" +
+            "● Convert the daysOfWeek immutable list into a mutable one. Remove all days containing\n" +
+            "the letter ‘n’, then print the mutable list.\n")
+    val daysOfWeek2 = mutableListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    daysOfWeek2
+        .removeAll{ it.contains("n", ignoreCase = true) }
+    println(daysOfWeek2)
+    println("\n")
+    println("● Print each element of the list in a new line together with the index of the element (convert\n" +
+            "the list to list with index using the withIndex() function!)\n")
+    daysOfWeek2.withIndex().forEach { (index, day) ->
+        println("Item at " + index + " is " + day)
+    }
+    println("\n")
+    println("● Sort the result list alphabetically!\n")
+    daysOfWeek2
+        .removeAll{ it.contains("n", ignoreCase = true) }
+    daysOfWeek2.sort()
+    println(daysOfWeek2)
+    println("\n")
+    println("8. Arrays.\n" +
+            "● Generate an array of 10 random integers between 0 and 100, and use forEach to print\n" +
+            "each element of the array in a new line.\n")
+    val randomNumbers = IntArray(10) { Random.nextInt(0, 101) }
+    randomNumbers.forEach { println(it) }
+    println("\n")
+    println("● Print the array sorted in ascending order!\n")
+    val sortedNumbers = randomNumbers.sorted()
+    sortedNumbers.forEach { println(it) }
+    println("\n")
+    println("● Check whether the array contains any even number!\n")
+    val evenNumbers = randomNumbers.filter { it % 2 == 0 }
+    if (evenNumbers.isNotEmpty()) {
+        println("The array contains at least one even number.\n")
+        evenNumbers.forEach { println(it) }
+    } else {
+        println("The array does not contain any even numbers.\n")
+    }
+    println("\n")
+    println("● Check whether all the numbers are even!\n")
+    val allEven = randomNumbers.all { it % 2 == 0 }
+    randomNumbers.forEach { println(it) }
+    if (allEven){
+        println("All numbers is even!\n")
+    } else {
+        println("Not all numbers are even!\n")
+    }
+    println("\n")
+    println("● Calculate the average of generated numbers and print it using forEach!\n")
+    val average = randomNumbers.average()
+    listOf(average).forEach { println("The average of the generated numbers is (using forEach): $it") }
 }
 
 fun isPrime(num: Int): Boolean {
@@ -118,4 +184,8 @@ fun messageCoding(msg: String, func: (String) -> String): String {
 
 fun isEvenCompact(number : Int) : Boolean = number % 2 == 0
 fun printEvenNumbers(numbers: List<Int>) = numbers.filter { it % 2 == 0 }.forEach { println(it) }
-fun doubleElements(numbers: List<Int>) = numbers.map { it * 2 }
+fun doubleElements(numbers: List<Int>) = numbers.map { it * 2 }.forEach{ println(it) }
+fun capitalizedLetters(days: List<String>) = days.map { it.uppercase() }.forEach { println(it) }
+fun firstLetter(days: List<String>) = days.map { it.first().lowercase() }.forEach{ println(it)}
+fun sizeLetters(days: List<String>) = days.map { it.length }.forEach{ println(it) }
+fun averageLength(days: List<String>): Double = days.map { it.length }.average()
